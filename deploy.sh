26ac7bd7bc76fd63fcd527aff10b18570b51b1c4
Gino Labiste <gino.labiste@gmail.com>
2022-10-20 08:52:43 +0800

    for depoyment

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ginolabiste/weight-tracker-vue.git main:gh-pages
+git push -f git@github.com:ginolabiste/weight-tracker-vue.git main
 cd -
